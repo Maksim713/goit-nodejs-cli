37f343c3d31528ae9a9eace354ba629cc83ba693
Maxim Sytnik <118841889+Maksim713@users.noreply.github.com>
2023-10-24 16:02:34 +0300

    fixed cli

--- contacts.js
+++ contacts.js
@@ -5,34 +5,38 @@ const { nanoid } = require("nanoid");
 const contactsPath = path.resolve("./db/contacts.json");
 
 async function listContacts() {
-  const data = await fs.readFile(contactsPath, "utf-8");
-  return JSON.parse(data);
+  return await fs
+    .readFile(contactsPath, "utf-8")
+    .then((r) => JSON.parse(r))
+    .catch(console.error);
 }
 
 async function getContactById(contactId) {
-  const list = await listContacts();
+  const list = await listContacts().catch(console.error);
   const contact = list.find((item) => item.id === contactId);
   return contact || null;
 }
 
 async function removeContact(contactId) {
-  const list = await listContacts();
+  const list = await listContacts().catch(console.error);
   const index = list.findIndex((item) => item.id === contactId);
   if (index === -1) {
     return null;
   }
   const removableContact = list[index];
   list.splice(index, 1);
-  await fs.writeFile(contactsPath, JSON.stringify(list, null, 2));
+  await fs.writeFile(contactsPath, JSON.stringify(list, null, 2)).catch();
   return removableContact;
 }
 
 async function addContact(name, email, phone) {
-  const list = await listContacts();
+  const list = await listContacts().catch(console.error);
   const id = nanoid(5);
   const newContact = { id, name, email, phone };
   const newList = [...list, newContact];
-  await fs.writeFile(contactsPath, JSON.stringify(newList, null, 2));
+  await fs
+    .writeFile(contactsPath, JSON.stringify(newList, null, 2))
+    .catch(console.error);
   return newContact;
 }
 
